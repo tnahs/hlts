d66518495b434323c5149f0744108bc7dae653a8
tnahs <31777797+tnahs@users.noreply.github.com>
2018-10-20 17:47:32 +0200

    Reworked beta page structure

--- app/beta/views.py
+++ app/beta/views.py
@@ -6,14 +6,24 @@ from flask import render_template
 from flask_login import login_required
 
 
-MARKDOWN_FOLDER = os.path.join(beta.root_path + "/markdown/")
+PAGES_FOLDER = os.path.join(beta.root_path + "/pages/")
 
 
 @beta.route("/")
 @login_required
 def welcome():
 
-    with open(MARKDOWN_FOLDER + "welcome.md", "r") as f:
+    with open(PAGES_FOLDER + "welcome.md", "r") as f:
+        content = f.read()
+
+    return render_template("beta/beta.html", content=content)
+
+
+@beta.route("/manual")
+@login_required
+def manual():
+
+    with open(PAGES_FOLDER + "manual.md", "r") as f:
         content = f.read()
 
     return render_template("beta/beta.html", content=content)
@@ -23,7 +33,7 @@ def welcome():
 @login_required
 def markdown():
 
-    with open(MARKDOWN_FOLDER + "using_markdown.md", "r") as f:
+    with open(PAGES_FOLDER + "markdown.md", "r") as f:
         content = f.read()
 
     return render_template("beta/beta.html", content=content)
